05d667a9fecf74782a7c3bb4d77fb0e63216621f
peng_zhang <zhangpeng@syberos.com>
2016-03-22 11:33:24 +0800

    新建faceme
    [提交原因] 新功能
    [详细描述]
    新建工程faceme
    [其它]

---
+++ faceme/src/faceme_Workspace.cpp
@@ -0,0 +1,32 @@
+#include "faceme_Workspace.h"
+#include <QDebug>
+
+faceme_Workspace::faceme_Workspace()
+    : CWorkspace()
+{
+    m_view = SYBEROS::SyberosGuiCache::qQuickView();
+    QObject::connect(m_view->engine(), SIGNAL(quit()), qApp, SLOT(quit()));
+    m_view->setSource(QUrl("qrc:/qml/main.qml"));
+    m_view->showFullScreen();
+}
+
+void faceme_Workspace::onLaunchComplete(Option option, const QStringList& params)
+{
+    Q_UNUSED(params)
+
+    switch (option) {
+    case CWorkspace::HOME:
+        qDebug()<< "Start by Home";
+        break;
+    case CWorkspace::URL:
+        break;
+    case CWorkspace::EVENT:
+        break;
+    case CWorkspace::DOCUMENT:
+        break;
+    default:
+        break;
+    }
+}
+
+
---
+++ faceme/src/main.cpp
@@ -0,0 +1,8 @@
+#include "faceme_Workspace.h"
+
+
+Q_DECL_EXPORT int main(int argc, char *argv[])
+{
+    return SYBEROS::SyberosGuiCache::qApplication(argc, argv, new faceme_Workspace());
+}
+
